60d2b2f50fb586076111bb9b5151d8ef885d9ba7
Winston Sung <winston21.sung@gmail.com>
2026-01-22 17:16:22 +0800

    Update scripts
    Change-Id: I07894623a07278ac9828d8414640e5aa929120ed

--- resources/site.v1.scripts/scripts.js
+++ resources/site.v1.scripts/scripts.js
@@ -6,137 +6,6 @@ window.site.config.page_path ??= document.body?.getAttribute( "data-site-page-pa
 
 window.site.config.page_view_lang ??= document.body?.getAttribute( "data-site-page-view-lang" );
 
-window.site.onMainPage ??= function () {
-};
-
-window.site.onInstructions ??= function () {
-};
-
-window.site.onQuiz ??= function () {
-};
-
-window.site.onResults ??= function () {
-    window.site.onResultsRadarChart();
-};
-
-window.site.onResultsRadarChart ??= function () {
-    const radarChartValues = Object.fromEntries(
-        Array.from(
-            new URLSearchParams( window.location.search )
-        ).map( ( [ k, v ] ) =>
-            [ k, Number( v ) ]
-        ).filter( ( [ _, v ] ) =>
-            !Number.isNaN( v ) && v >= 0 && v <= 5
-        )
-    );
-
-    if (
-        !( "n" in radarChartValues )
-        || !( "i" in radarChartValues )
-        || !( "r" in radarChartValues )
-        || !( "a" in radarChartValues )
-        || !( "p" in radarChartValues )
-        || !( "s" in radarChartValues )
-    ) {
-        if (
-            false /* noscript query */
-        ) {
-        } else if (
-            false /* ongoing quiz */
-        ) {
-            location = "./../quiz/";
-        } else {
-            location = "./../";
-        }
-    }
-
-    const radarChart = echarts.init(
-        document.getElementById( "result_chart" ),
-        null,
-        {
-            renderer: "svg",
-        }
-    );
-
-    const radarChartOption = {
-        title: {
-            text: "健康生活型態雷達圖",
-            textStyle: {
-                fontSize: 20,
-            },
-        },
-        tooltip: {},
-        radar: [
-            {
-                axisName: {
-                    fontSize: 16,
-                },
-                indicator: [
-                    {
-                        /* n: proper nutrition */
-                        text: "適當的營養",
-                        max: 5,
-                    },
-                    {
-                        /* i: interpersonal support */
-                        text: "人際支持",
-                        max: 5,
-                    },
-                    {
-                        /* r: health responsibility */
-                        text: "健康責任",
-                        max: 5,
-                    },
-                    {
-                        /* a: self-actualization */
-                        text: "自我實現",
-                        max: 5,
-                    },
-                    {
-                        /* p: physical activity */
-                        text: "運動",
-                        max: 5,
-                    },
-                    {
-                        /* s: stress management */
-                        text: "壓力處理",
-                        max: 5,
-                    },
-                ],
-            },
-        ],
-        series: [
-            {
-                type: "radar",
-                tooltip: {},
-                areaStyle: {},
-                data: [
-                    {
-                        name: "健康生活型態雷達圖",
-                        value: [
-                            radarChartValues.n,
-                            radarChartValues.i,
-                            radarChartValues.r,
-                            radarChartValues.a,
-                            radarChartValues.p,
-                            radarChartValues.s,
-                        ],
-                    },
-                ],
-            },
-        ],
-    };
-
-    radarChart.setOption( radarChartOption );
-
-    window.addEventListener(
-        "resize",
-        () => {
-            radarChart.resize();
-        }
-    );
-};
-
 window.site.addOnClickListener ??= function () {
     document.body.addEventListener(
         "click",
@@ -186,67 +55,198 @@ window.site.addOnClickListener ??= function () {
     );
 };
 
-window.site.singlePageApplicationPageData ??= {
-    "zh-Hant": {
-        "title": "健康生活型態測驗 Healthy Lifestyle Test",
-        "heading": "首頁",
-        "content": "",
-    },
-    "zh-Hant/instructions": {
-        "title": "測驗說明 | 健康生活型態測驗 Healthy Lifestyle Test",
-        "heading": "測驗說明",
-        "content": "",
-    },
-    "zh-Hant/quiz": {
-        "title": "測驗 | 健康生活型態測驗 Healthy Lifestyle Test",
-        "heading": "測驗",
-        "content": "",
-    },
-    "zh-Hant/results": {
-        "title": "測驗結果 | 健康生活型態測驗 Healthy Lifestyle Test",
-        "heading": "測驗結果",
-        "content": "",
-    },
-};
-
-window.site.singlePageApplicationPageHooks ??= {
-    "zh-Hant": window.site.onMainPage,
-    "zh-Hant/instructions": window.site.onInstructions,
-    "zh-Hant/quiz": window.site.onQuiz,
-    "zh-Hant/results": window.site.onResults,
-};
-
 window.site.renderSinglePageApplicationPage ??= function ( page_path ) {
     const firstHeading = document.getElementById("firstHeading");
+
+    const singlePageApplicationPageData = {
+        "zh-Hant": {
+            "title": "健康生活型態測驗 Healthy Lifestyle Test",
+            "heading": "首頁",
+            "content": "",
+        },
+        "zh-Hant/instructions": {
+            "title": "測驗說明 | 健康生活型態測驗 Healthy Lifestyle Test",
+            "heading": "測驗說明",
+            "content": "",
+        },
+        "zh-Hant/quiz": {
+            "title": "測驗 | 健康生活型態測驗 Healthy Lifestyle Test",
+            "heading": "測驗",
+            "content": "",
+        },
+        "zh-Hant/results": {
+            "title": "測驗結果 | 健康生活型態測驗 Healthy Lifestyle Test",
+            "heading": "測驗結果",
+            "content": "",
+        },
+    };
 
     if (
         firstHeading === null
         || typeof( page_path ) !== "string"
-        || !( page_path in window.site.singlePageApplicationPageData )
-        || !( "title" in window.site.singlePageApplicationPageData[page_path] )
-        || typeof( window.site.singlePageApplicationPageData[page_path].title ) !== "string"
-        || !( "content" in window.site.singlePageApplicationPageData[page_path] )
-        || typeof( window.site.singlePageApplicationPageData[page_path].content ) !== "string"
+        || !( page_path in singlePageApplicationPageData )
+        || !( "title" in singlePageApplicationPageData[page_path] )
+        || typeof( singlePageApplicationPageData[page_path].title ) !== "string"
+        || !( "content" in singlePageApplicationPageData[page_path] )
+        || typeof( singlePageApplicationPageData[page_path].content ) !== "string"
     ) {
         return;
     }
 
-    document.title = window.site.singlePageApplicationPageData[page_path].title;
-    firstHeading.innerText = window.site.singlePageApplicationPageData[page_path].heading;
-    /* content = window.site.singlePageApplicationPageData[page_path].content; */
+    document.title = singlePageApplicationPageData[page_path].title;
+    firstHeading.innerText = singlePageApplicationPageData[page_path].heading;
+    /* content = singlePageApplicationPageData[page_path].content; */
     window.site.onRenderSinglePageApplicationPage( page_path );
 };
 
 window.site.onRenderSinglePageApplicationPage ??= function ( page_path ) {
+    const onMainPage = function () {
+    };
+
+    const onInstructions = function () {
+    };
+
+    const onQuiz = function () {
+    };
+
+    const onResultsRadarChart = function () {
+        const radarChartValues = Object.fromEntries(
+            Array.from(
+                new URLSearchParams( window.location.search )
+            ).map( ( [ k, v ] ) =>
+                [ k, Number( v ) ]
+            ).filter( ( [ _, v ] ) =>
+                !Number.isNaN( v ) && v >= 0 && v <= 5
+            )
+        );
+
+        if (
+            !( "n" in radarChartValues )
+            || !( "i" in radarChartValues )
+            || !( "r" in radarChartValues )
+            || !( "a" in radarChartValues )
+            || !( "p" in radarChartValues )
+            || !( "s" in radarChartValues )
+        ) {
+            if (
+                false /* noscript query */
+            ) {
+            } else if (
+                false /* ongoing quiz */
+            ) {
+                location = "./../quiz/";
+            } else {
+                location = "./../";
+            }
+        }
+
+        const radarChart = echarts.init(
+            document.getElementById( "result_chart" ),
+            null,
+            {
+                renderer: "svg",
+            }
+        );
+
+        const radarChartOption = {
+            title: {
+                text: "健康生活型態雷達圖",
+                textStyle: {
+                    fontSize: 20,
+                },
+            },
+            tooltip: {},
+            radar: [
+                {
+                    axisName: {
+                        fontSize: 16,
+                    },
+                    indicator: [
+                        {
+                            /* n: proper nutrition */
+                            text: "適當的營養",
+                            max: 5,
+                        },
+                        {
+                            /* i: interpersonal support */
+                            text: "人際支持",
+                            max: 5,
+                        },
+                        {
+                            /* r: health responsibility */
+                            text: "健康責任",
+                            max: 5,
+                        },
+                        {
+                            /* a: self-actualization */
+                            text: "自我實現",
+                            max: 5,
+                        },
+                        {
+                            /* p: physical activity */
+                            text: "運動",
+                            max: 5,
+                        },
+                        {
+                            /* s: stress management */
+                            text: "壓力處理",
+                            max: 5,
+                        },
+                    ],
+                },
+            ],
+            series: [
+                {
+                    type: "radar",
+                    tooltip: {},
+                    areaStyle: {},
+                    data: [
+                        {
+                            name: "健康生活型態雷達圖",
+                            value: [
+                                radarChartValues.n,
+                                radarChartValues.i,
+                                radarChartValues.r,
+                                radarChartValues.a,
+                                radarChartValues.p,
+                                radarChartValues.s,
+                            ],
+                        },
+                    ],
+                },
+            ],
+        };
+
+        radarChart.setOption( radarChartOption );
+
+        window.addEventListener(
+            "resize",
+            () => {
+                radarChart.resize();
+            }
+        );
+    };
+
+    const onResults = function () {
+        onResultsRadarChart();
+    };
+
+    const singlePageApplicationPageHooks = {
+        "zh-Hant": window.site.onMainPage,
+        "zh-Hant/instructions": window.site.onInstructions,
+        "zh-Hant/quiz": window.site.onQuiz,
+        "zh-Hant/results": window.site.onResults,
+    };
+
     if (
         typeof( page_path ) !== "string"
-        || !( page_path in window.site.singlePageApplicationPageHooks )
-        || typeof( window.site.singlePageApplicationPageHooks[page_path] ) !== "function"
+        || !( page_path in singlePageApplicationPageHooks )
+        || typeof( singlePageApplicationPageHooks[page_path] ) !== "function"
     ) {
         return;
     }
 
-    ( window.site.singlePageApplicationPageHooks[page_path] )();
+    ( singlePageApplicationPageHooks[page_path] )();
 };
 
 /*
@@ -277,10 +277,9 @@ window.site.addOnClickListener ??= function () {
 ( function () {
     const page_path = window.site?.config?.page_path;
 
-    if (
-        typeof( page_path ) === "string"
-        && page_path in window.site.singlePageApplicationPageHooks
-    ) {
-        window.site.onRenderSinglePageApplicationPage( page_path );
+    if ( typeof( page_path ) !== "string" ) {
+        return;
     }
+
+    window.site.onRenderSinglePageApplicationPage( page_path );
 } ) ();
